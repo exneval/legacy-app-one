89208c4afe20924f9cb92899c760ff289539ebfe
exneval <exneval.rayz@gmail.com>
2023-07-04 10:24:40 +0700

    chore: support build with docker
    Signed-off-by: exneval <exneval.rayz@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import fs from 'fs';
+import react from "@vitejs/plugin-react";
+import path from "path";
+import fs from "fs";
 
 // In codesandbox, we won't have the packages folder
 // We ignore errors in this case
 let aliases = [];
 try {
-    const packages = fs.readdirSync(path.resolve(__dirname, '../../packages'));
-    aliases = packages.map(dirName => {
-        const packageJson = require(path.resolve(
-            __dirname,
-            '../../packages',
-            dirName,
-            'package.json'
-        ));
-        return {
-            find: new RegExp(`^${packageJson.name}$`),
-            replacement: path.resolve(
-                __dirname,
-                `../../packages/${packageJson.name}/src`
-            ),
-        };
-    }, {});
+  const packages = fs.readdirSync(path.resolve(__dirname, "../../packages"));
+  aliases = packages.map((dirName) => {
+    const packageJson = require(path.resolve(
+      __dirname,
+      "../../packages",
+      dirName,
+      "package.json"
+    ));
+    return {
+      find: new RegExp(`^${packageJson.name}$`),
+      replacement: path.resolve(
+        __dirname,
+        `../../packages/${packageJson.name}/src`
+      ),
+    };
+  }, {});
 } catch {}
 
 /**
@@ -29,18 +29,19 @@ try {
  * @type { import('vite').UserConfig }
  */
 export default {
-    plugins: [react()],
-    resolve: {
-        alias: [
-            ...aliases,
-            {
-                find: /^@mui\/icons-material\/(.*)/,
-                replacement: '@mui/icons-material/esm/$1',
-            },
-        ],
-    },
-    server: {
-        port: 8080,
-    },
-    define: { 'process.env': {} },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      ...aliases,
+      {
+        find: /^@mui\/icons-material\/(.*)/,
+        replacement: "@mui/icons-material/esm/$1",
+      },
+    ],
+  },
+  preview: {
+    host: true,
+    strictPort: true,
+  },
+  define: { "process.env": {} },
 };
